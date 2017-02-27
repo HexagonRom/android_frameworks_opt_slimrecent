14a9572454bfffb1c044e9f86e6577dfcbebacd2
SpiritCroc <spiritcroc@gmail.com>
2017-02-27 15:05:13 +0100

    Slim recents AICP empty view: don't show after clear all
    I disabled it previously for removing the last app, but forgot to do
    the same when using the pinch gesture.
    
    Our empty recents animation is too good to view in such a short time :P
    
    Change-Id: I728c15f847cf9b8dbe1f923238ac2cc65083f9dc

--- src/com/android/systemui/slimrecent/RecentController.java
+++ src/com/android/systemui/slimrecent/RecentController.java
@@ -853,7 +853,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                 });
 
                 // Setup animation for empty recent image - fade in.
-                if (!hasFavorite) {
+                if (!hasFavorite && mHexEmptyView) {
                     mEmptyRecentView.setAlpha(0.0f);
                     mEmptyRecentView.setVisibility(View.VISIBLE);
                 }
@@ -869,7 +869,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                 // Start all ValueAnimator animations
                 // and listen onAnimationEnd to prepare the views for the next call.
                 AnimatorSet animationSet = new AnimatorSet();
-                if (hasFavorite) {
+                if (hasFavorite || mHexEmptyView) {
                     animationSet.playTogether(animation1, animation3);
                 } else {
                     animationSet.playTogether(animation1, animation2, animation3, animation4);
