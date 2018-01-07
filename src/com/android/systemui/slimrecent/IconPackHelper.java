AICP/frameworks_opt_slimrecent

Slim recent app sidebar icon pack support [2/2]
Change-Id: Idc7c8b7d225cae6050b1be6c108464938a263c30

src/com/android/systemui/slimrecent/IconPackHelper.java
@@ -29,6 +29,8 @@ import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 
+import com.android.internal.util.aicp.AbstractIconPackHelper;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -41,7 +43,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-public class IconPackHelper {
+public class IconPackHelper implements AbstractIconPackHelper {
     static final String ICON_MASK_TAG = "iconmask";
     static final String ICON_BACK_TAG = "iconback";
     static final String ICON_UPON_TAG = "iconupon";

HexagonRom/android_frameworks_opt_slimrecent

src/com/android/systemui/slimrecent/IconPackHelper.java
@@ -29,6 +29,8 @@ import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
 import android.text.TextUtils;
 
+import com.android.internal.util.hex.AbstractIconPackHelper;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
@@ -41,7 +43,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-public class IconPackHelper {
+public class IconPackHelper implements AbstractIconPackHelper {
     static final String ICON_MASK_TAG = "iconmask";
     static final String ICON_BACK_TAG = "iconback";
     static final String ICON_UPON_TAG = "iconupon";